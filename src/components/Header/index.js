b09b94c88f763f6aef5ca642b5109ccddfb4b0c0
Fernando Montoya <montogeek@gmail.com>
2016-11-28 20:43:27 -0500

    Added buy tickets button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -19,13 +19,16 @@ export default class Hero extends Component {
       <div className="gradient">
         <div id="particles-js" className="mw80 w-100 vh-90 center relative">
           <header className="absolute pa4 w-100 tc">
-            <h1 className="ttu bg-white green1 f-1 mt6 eau-book">ScaleConf</h1>
+            <h1 className="ttu bg-white pa1 green1 f-1 mt6 eau-book">ScaleConf</h1>
             <p className="ttu green2 pv3 ph4 pt3 f-3 mt0 open-sans">
               The first Colombian tech event focused on Scalability.
             </p>
             <p className="ttu white pa3 f3 lh-copy tracked eau-bold">
               Medellín, <span className="underline">Colombia</span> March 25 2017
             </p>
+            <a className="bg-blue1 mt1 white pa3">
+              Buy your tickets now
+            </a>
           </header>
         </div>
       </div>
