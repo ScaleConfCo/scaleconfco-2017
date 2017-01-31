b7d2a178feacc552245af18566c9b33800a7c7bd
Fernando Montoya <montogeek@gmail.com>
2017-01-31 15:02:13 -0500

    Fix lint issues

--- src/fragments/Schedule.js
+++ src/fragments/Schedule.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { PropTypes, Component } from 'react'
 
 const day1 = [
 	{
@@ -32,63 +32,59 @@ const day2 = [
   }
 ]
 
-class TimelineItem extends Component {
-  render() {
-    const {
-      hour,
-      title,
-      speaker,
-      description
-    } = this.props.item
-
-    return (
-      <li className="relative timeline-item h-100">
-        {
-          hour &&
-          <time className="ml4 bright-green open-sans f-1-25">{hour}</time>
-        }
-        <p className="ml4 bright-green ttu eau-book f-4">{title}</p>
-        {
-          speaker &&
-          <p className="ml4 bright-green ttu open-sans f-s-d">{speaker}</p>
-        }
-        <p className="ml4 white open-sans">{description}</p>
-      </li>
-    )
-  }
+const TimelineItem = ({ item }) => {
+  const { hour, title, speaker, description } = item
+  return (
+    <li className="relative timeline-item h-100">
+      {
+        hour &&
+        <time className="ml4 bright-green open-sans f-1-25">{hour}</time>
+      }
+      <p className="ml4 bright-green ttu eau-book f-4">{title}</p>
+      {
+        speaker &&
+        <p className="ml4 bright-green ttu open-sans f-s-d">{speaker}</p>
+      }
+      <p className="ml4 white open-sans">{description}</p>
+    </li>
+  )
 }
 
-class Timeline extends Component {
-  render() {
-    const { schedule } = this.props
-    return (
-      <ul className="list relative pa0 timeline">
-        {
-          schedule.map((item, index) => <TimelineItem item={item} key={index} />)
-        }
-      </ul>
-    )
-  }
+TimelineItem.propTypes = {
+  item: PropTypes.object.isRequired
 }
 
-class Day extends Component {
-  render() {
-    const {
-      date,
-      schedule,
-      index
-    } = this.props
-    return (
-      <div className="flex-auto w-50">
-        <div>
-          <p className="bright-green ttu eau-bold f-1-75 mb1">Day { index }</p>
-          <p className="white ttu eau-book f-1-125 mt0">{date}</p>
-          <hr className="h-4 bg-bright-green b-n"/>
-          <Timeline schedule={schedule} />
-        </div>
+const Timeline = ({ schedule }) => {
+  return (
+    <ul className="list relative pa0 timeline">
+      {
+        schedule.map((item, index) => <TimelineItem item={item} key={index} />)
+      }
+    </ul>
+  )
+}
+
+Timeline.propTypes = {
+  schedule: PropTypes.object.isRequired
+}
+
+const Day = ({ date, schedule, index }) => {
+  return (
+    <div className="flex-auto w-50">
+      <div>
+        <p className="bright-green ttu eau-bold f-1-75 mb1">Day { index }</p>
+        <p className="white ttu eau-book f-1-125 mt0">{date}</p>
+        <hr className="h-4 bg-bright-green b-n"/>
+        <Timeline schedule={schedule} />
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+Day.propTypes = {
+  date: PropTypes.string.isRequired,
+  schedule: PropTypes.array.isRequired,
+  index: PropTypes.string.isRequired
 }
 
 class Schedule extends Component {
