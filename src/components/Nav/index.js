05c9776cea818480c12b2636399f219ee0adda11
Jonathan David Avendaño Ortega <jdavidavendano@gmail.com>
2017-03-03 21:59:14 -0500

    Nav link and background fix

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -17,7 +17,7 @@ export default class Nav extends React.Component {
     const { open } = this.state
     const openClass = !open ? 'dn': '';
     return (
-      <div className="fixed left-0 right-0 top-0 bg-blue1 z-999">
+      <div className="fixed left-0 right-0 top-0 bg-blue1 z-999 gradient">
         {
           this.state.open &&
           <a onClick={this.toggle} className="white f1 tr db dn-l ph2 pointer">&#x2715;</a>
@@ -30,25 +30,25 @@ export default class Nav extends React.Component {
           <nav className="bg-blue1 vh-100 h-auto-l" style={{'fontSize': 0}}>
             <ul className="list tc pl0 eau-bold flex justify-between flex-column flex-row-l h-75">
               <li className="pv3">
-                <a onClick={this.toggle} href="/#about" className="nav__opt">About</a>
+                <a onClick={this.toggle} href="#about" className="nav__opt">About</a>
               </li>
               <li className="pv3">
-                <a onClick={this.toggle} href="/#speakers" className="nav__opt">Speakers</a>
+                <a onClick={this.toggle} href="#speakers" className="nav__opt">Speakers</a>
               </li>
               <li className="pv3">
-                <a onClick={this.toggle} href="/#schedule" className="nav__opt">Schedule</a>
+                <a onClick={this.toggle} href="#schedule" className="nav__opt">Schedule</a>
               </li>
               <li className="pv3">
-                <a onClick={this.toggle} href="/#sponsors" className="nav__opt">Sponsors</a>
+                <a onClick={this.toggle} href="#sponsors" className="nav__opt">Sponsors</a>
               </li>
               <li className="pv3">
-                <a onClick={this.toggle} href="/#venue" className="nav__opt">Venue</a>
+                <a onClick={this.toggle} href="#venue" className="nav__opt">Venue</a>
               </li>
               <li className="pv3">
-                <a onClick={this.toggle} href="/#scholarships" className="nav__opt">Scholarships</a>
+                <a onClick={this.toggle} href="#scholarships" className="nav__opt">Scholarships</a>
               </li>
               <li className="pv3">
-                <a onClick={this.toggle} href="/#codeofconduct" className="nav__opt">Code of conduct</a>
+                <a onClick={this.toggle} href="#codeofconduct" className="nav__opt">Code of conduct</a>
               </li>
             </ul>
           </nav>
